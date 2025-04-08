0dc9084f0d3c1ea981f94e8007f9cf5ecf710eed
Prethiveerajan05 <raj0105200@gmail.com>
2025-04-08 23:24:20 +0530

    Stop tracking .env file

--- src/models/Application.js
+++ src/models/Application.js
@@ -1,18 +1,5 @@
 const mongoose = require("mongoose");
 
-// const applicationSchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true },
-//     education: { type: String, required: true },
-//     degree: { type: String, required: true },
-//     resume: { type: String, required: true }, // Assuming you have a Job model
-//   },
-//   { timestamps: true }
-// );
-
-// const Application = mongoose.model("Application", applicationSchema);
-
-// module.exports = Application;
 
 const applicationSchema = new mongoose.Schema({
   name: { type: String, required: true },
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,21 +1,4 @@
-// const express = require("express");
-// const { register, login, logout, getUser,forgotPassword,resetPassword } = require("../controllers/authController");
-// const { protect } = require("../middleware/authMiddleware");
 
-// const router = express.Router();
-
-// router.post("/login", login); 
-// router.post("/forgot-password", forgotPassword);
-// router.post("/signup", register);  
-// router.post("/logout", logout);
-// router.get("/me", protect, getUser);
-// // router.post("/forgot-password", forgotPassword);
-// router.post("/reset-password/:token", resetPassword);
-// router.post("/reset-password/:token", resetPassword);
-
-
-
-// module.exports = router;
 
 const express = require("express");
 const { register, login, logout, getUser, forgotPassword, resetPassword } = require("../controllers/authController");
